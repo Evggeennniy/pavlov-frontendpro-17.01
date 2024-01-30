5dfdac9ba1f3c9139017c9b6c46f686f9a603ef8
FiX <myiphone128@mail.ru>
2024-01-30 18:50:15 +0200

    homework8

--- main.js
+++ main.js
@@ -116,9 +116,74 @@ const averageNumber = (...args) => {
   return args.reduce((total, num) => total + num, 0) / args.length;
 }; // Решил сделать более универсальный вариант но не до конца разобрался в методе reduce
 
-const num1 = parseFloat(prompt("input number 1"));
-const num2 = parseFloat(prompt("input number 2"));
-const num3 = parseFloat(prompt("input number 3"));
+// const num1 = parseFloat(prompt("input number 1"));
+// const num2 = parseFloat(prompt("input number 2"));
+// const num3 = parseFloat(prompt("input number 3"));
 
-const result = averageNumber(num1, num2, num3);
-alert(`average number is ${result}`);
+// const result = averageNumber(num1, num2, num3);
+// alert(`average number is ${result}`);
+
+// Homework 8
+// 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+const createRange = function (pointA, pointB, step = 1) {
+  const result = new Array();
+  while (pointA <= pointB) {
+    result.push(pointA);
+    pointA += step;
+  }
+  return result;
+};
+const resultbyTask1 = createRange(20, 30, 0.5).join(" ");
+console.log(resultbyTask1);
+
+// 2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+const dollarPrice = 27;
+const resultbyTask2 = createRange(10, 100, 10);
+resultbyTask2.forEach((element, index, array) => {
+  array[index] = element * dollarPrice;
+});
+console.log(resultbyTask2);
+
+// 3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+const findNumbers = function (number) {
+  const numbersArray = new Array();
+  let numPossition = 1;
+  while (numPossition <= 100) {
+    if (numPossition ** 2 <= number) {
+      numbersArray.push(numPossition);
+      numPossition++;
+    } else {
+      break;
+    }
+  }
+  return numbersArray;
+};
+const resultbyTask3 = findNumbers(200);
+console.log(resultbyTask3);
+
+// 4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
+const isPrime = (number) => {
+  if (number <= 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+
+  return true;
+};
+const resultbyTask4 = isPrime(5);
+console.log(resultbyTask4);
+
+// 5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
+const ismadebyThree = (number) => {
+  if (number <= 0) return false;
+
+  while (number % 3 == 0) {
+    number /= 3;
+  }
+
+  return number == 1;
+};
+
+const resultbyTask5 = ismadebyThree(13);
+console.log(resultbyTask5);
