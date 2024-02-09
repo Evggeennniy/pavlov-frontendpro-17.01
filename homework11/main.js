be9f7fd942e78484aa7c4a9e6afc267506fa07f8
FiX <myiphone128@mail.ru>
2024-02-09 08:04:39 +0200

    fixed homework11 function

--- homework11/main.js
+++ homework11/main.js
@@ -1,6 +1,9 @@
 // Homework11
 const removeElement = (array, element) => {
-  delete array[array.indexOf(element)];
+  const index = array.indexOf(element);
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
 };
 
 const array = [1, 2, 3, 4, 5, 6, 7];
