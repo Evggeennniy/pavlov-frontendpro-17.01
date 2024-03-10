bb70f4dee224091281dff88f1ea2e638324df9db
FiX <myiphone128@mail.ru>
2024-03-10 12:14:24 +0200

    homework25

---
+++ homework25/main.js
@@ -0,0 +1,17 @@
+const createList = (array) => {
+  const ul = document.createElement("ul");
+  for (let outerElement of array) {
+    const li = document.createElement("li");
+    if (typeof outerElement == "object") {
+      const innerUl = createList(outerElement);
+      li.append(innerUl);
+    } else {
+      li.innerHTML = outerElement;
+    }
+    ul.append(li);
+  }
+  return ul;
+};
+
+list = createList([1, 2, 3, [4.1, 4.2], 5, 6, [7.1, 7.2, [8.1, 8.2]]]);
+document.body.append(list);
