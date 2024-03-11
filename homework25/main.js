66be7fca5672c73710da23d51a53c7b9f598338e
FiX <myiphone128@mail.ru>
2024-03-11 20:14:38 +0200

    remake

--- homework25/main.js
+++ homework25/main.js
@@ -1,17 +1,31 @@
-const createList = (array) => {
+const createList = (array, depth = 0) => {
+  // Создаем ul и индекс для нумерации
   const ul = document.createElement("ul");
+  let index = 0;
+  // Начинаем обход массива
   for (let outerElement of array) {
+    // Создаем li
     const li = document.createElement("li");
     if (typeof outerElement == "object") {
-      const innerUl = createList(outerElement);
-      li.append(innerUl);
+      // Если объект является массивом вызываем себя
+      li.append(createList(outerElement, depth + 1));
     } else {
-      li.innerHTML = outerElement;
+      // В ином случае нумеруем объект
+      index++;
+      li.textContent = depth ? `${depth}.${index}` : index;
     }
+    // Добавляем сформированный элемент в ul
     ul.append(li);
   }
   return ul;
 };
 
-const list = createList([1, 2, 3, [4.1, 4.2], 5, 6, [7.1, 7.2, [8.1, 8.2]]]);
+const list = createList([
+  1,
+  2,
+  3,
+  [1, 2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, [1, 2, 3, [1, 2]]], 3, 4, 5],
+  4,
+  5,
+]);
 document.body.append(list);
