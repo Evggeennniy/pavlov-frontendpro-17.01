cd5de4ad50bad418d2d8f04cf6bbd47a1546c872
FiX <myiphone128@mail.ru>
2024-03-16 14:28:47 +0200

    homework27

--- homework27/main.js
+++ homework27/main.js
@@ -7,16 +7,29 @@ function showTable() {
   const form = document.getElementById("registrationForm");
   const userDataDiv = document.getElementById("userData");
   const formData = new FormData(form);
+  let allFieldsFilled = true;
 
-  let tableHTML = "<table>";
-  tableHTML += "<tr><th>Поле</th><th>Значення</th></tr>";
-
+  // Перевірка, чи всі поля форми заповнені
   for (const pair of formData.entries()) {
-    tableHTML += "<tr><td>" + pair[0] + "</td><td>" + pair[1] + "</td></tr>";
+    if (pair[1].trim() === "") {
+      allFieldsFilled = false;
+      break;
+    }
   }
 
-  tableHTML += "</table>";
+  if (allFieldsFilled) {
+    let tableHTML = "<table>";
+    tableHTML += "<tr><th>Поле</th><th>Значення</th></tr>";
 
-  userDataDiv.innerHTML = tableHTML;
-  form.reset(); // Очищення форми після відправлення
+    for (const pair of formData.entries()) {
+      tableHTML += "<tr><td>" + pair[0] + "</td><td>" + pair[1] + "</td></tr>";
+    }
+
+    tableHTML += "</table>";
+
+    userDataDiv.innerHTML = tableHTML;
+    form.reset(); // Очищення форми після відправлення
+  } else {
+    alert("Будь ласка, заповніть всі поля форми.");
+  }
 }
