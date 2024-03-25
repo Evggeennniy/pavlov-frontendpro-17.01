73e808759dce2ce686d091731c07f6fa9e1f083e
FiX <myiphone128@mail.ru>
2024-03-25 18:12:55 +0200

    fix

--- homework30/main.js
+++ homework30/main.js
@@ -71,7 +71,7 @@ weatherInfo.onreadystatechange = function () {
 // Открытие запроса (метод GET, адрес URL)
 weatherInfo.open(
   "GET",
-  "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19",
+  "https://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19",
   true
 );
 
